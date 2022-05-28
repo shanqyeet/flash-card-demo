4fe152d3fccb22cd23d1396aa4989f56348ce7c5
shanqyeet <shanqyeet@gmail.com>
2022-05-28 18:30:02 +0800

    first commit

---
+++ src/main/java/com/illumina/shanqyeet/flashcarddemo/FlashCardDemoApplication.java
@@ -0,0 +1,13 @@
+package com.illumina.shanqyeet.flashcarddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashCardDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashCardDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/illumina/shanqyeet/flashcarddemo/FlashCardDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.illumina.shanqyeet.flashcarddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashCardDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
